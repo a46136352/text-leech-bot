593d64f31e1e7d4bb0af46530c4ce903f8cb4169
a46136352 <a46136352@gmail.com>
2024-12-19 22:57:22 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -192,7 +192,7 @@ async def account_login(bot: Client, m: Message):
              url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
                 
             if "embed" in url:
-                ytf = f"bv[height<={raw_text2}]+ba/b[height<={raw_text2}]
+                ytf = f"bv[height<={raw_text2}]+ba/b[height<={raw_text2}]+ba/b/bv+ba"
             elif "youtube" in url:
                 ytf = f"bv[height<={raw_text2}][ext=mp4]+ba[ext=m4a]/b[height<={raw_text2}][ext=mp4]"
             else:
